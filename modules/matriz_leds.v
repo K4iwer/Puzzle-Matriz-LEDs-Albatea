3e05b23cef297195a14a80b048f4df50e752e4d7
ccPacha <pacha.c@usp.br>
2025-03-09 17:48:20 -0300

    Talvez haja merda

--- modules/matriz_leds.v
+++ modules/matriz_leds.v
@@ -68,71 +68,71 @@ module matriz_leds (
                 estado_leds[6][1] <= ~estado_leds[6][1];
             end
             if (botoes[2]) begin
-                estadoleds[4][2] <= ~estado_leds[4][2];
-                estadoleds[5][2] <= ~estado_leds[5][2];
-                estadoleds[5][3] <= ~estado_leds[5][3];
-                estadoleds[6][2] <= ~estado_leds[6][2];
-                estadoleds[6][3] <= ~estado_leds[6][3];
-                estadoleds[7][0] <= ~estado_leds[7][0];
-                estadoleds[7][1] <= ~estado_leds[7][1];
-                estadoleds[7][2] <= ~estado_leds[7][2];
-                estadoleds[7][3] <= ~estado_leds[7][3];
+                estado_leds[4][2] <= ~estado_leds[4][2];
+                estado_leds[5][2] <= ~estado_leds[5][2];
+                estado_leds[5][3] <= ~estado_leds[5][3];
+                estado_leds[6][2] <= ~estado_leds[6][2];
+                estado_leds[6][3] <= ~estado_leds[6][3];
+                estado_leds[7][0] <= ~estado_leds[7][0];
+                estado_leds[7][1] <= ~estado_leds[7][1];
+                estado_leds[7][2] <= ~estado_leds[7][2];
+                estado_leds[7][3] <= ~estado_leds[7][3];
             end
         
             if (botoes[3]) begin
-                estadoleds[5][3] <= ~estado_leds[5][3];
-                estadoleds[5][4] <= ~estado_leds[5][4];
-                estadoleds[5][5] <= ~estado_leds[5][5];
-                estadoleds[6][3] <= ~estado_leds[6][3];
-                estadoleds[6][4] <= ~estado_leds[6][4];
-                estadoleds[6][5] <= ~estado_leds[6][5];
-                estadoleds[7][3] <= ~estado_leds[7][3];
-                estadoleds[7][4] <= ~estado_leds[7][4];
-                estadoleds[7][5] <= ~estado_leds[7][5];
+                estado_leds[5][3] <= ~estado_leds[5][3];
+                estado_leds[5][4] <= ~estado_leds[5][4];
+                estado_leds[5][5] <= ~estado_leds[5][5];
+                estado_leds[6][3] <= ~estado_leds[6][3];
+                estado_leds[6][4] <= ~estado_leds[6][4];
+                estado_leds[6][5] <= ~estado_leds[6][5];
+                estado_leds[7][3] <= ~estado_leds[7][3];
+                estado_leds[7][4] <= ~estado_leds[7][4];
+                estado_leds[7][5] <= ~estado_leds[7][5];
             end
             if (botoes[4]) begin
-                estadoleds[0][3] <= ~estado_leds[0][3];
-                estadoleds[0][4] <= ~estado_leds[0][4];
-                estadoleds[1][3] <= ~estado_leds[1][3];
-                estadoleds[1][4] <= ~estado_leds[1][4];
-                estadoleds[2][3] <= ~estado_leds[2][3];
-                estadoleds[2][4] <= ~estado_leds[2][4];
-                estadoleds[3][3] <= ~estado_leds[3][3];
-                estadoleds[3][4] <= ~estado_leds[3][4];
-                estadoleds[4][3] <= ~estado_leds[4][3];
+                estado_leds[0][3] <= ~estado_leds[0][3];
+                estado_leds[0][4] <= ~estado_leds[0][4];
+                estado_leds[1][3] <= ~estado_leds[1][3];
+                estado_leds[1][4] <= ~estado_leds[1][4];
+                estado_leds[2][3] <= ~estado_leds[2][3];
+                estado_leds[2][4] <= ~estado_leds[2][4];
+                estado_leds[3][3] <= ~estado_leds[3][3];
+                estado_leds[3][4] <= ~estado_leds[3][4];
+                estado_leds[4][3] <= ~estado_leds[4][3];
             end
             if (botoes[5]) begin
-                estadoleds[0][5] <= ~estado_leds[0][5];
-                estadoleds[1][5] <= ~estado_leds[1][5];
-                estadoleds[2][5] <= ~estado_leds[2][5];
-                estadoleds[2][6] <= ~estado_leds[2][6];
-                estadoleds[3][5] <= ~estado_leds[3][5];
-                estadoleds[3][6] <= ~estado_leds[3][6];
-                estadoleds[4][4] <= ~estado_leds[4][4];
-                estadoleds[4][5] <= ~estado_leds[4][5];
-                estadoleds[4][6] <= ~estado_leds[4][6];
+                estado_leds[0][5] <= ~estado_leds[0][5];
+                estado_leds[1][5] <= ~estado_leds[1][5];
+                estado_leds[2][5] <= ~estado_leds[2][5];
+                estado_leds[2][6] <= ~estado_leds[2][6];
+                estado_leds[3][5] <= ~estado_leds[3][5];
+                estado_leds[3][6] <= ~estado_leds[3][6];
+                estado_leds[4][4] <= ~estado_leds[4][4];
+                estado_leds[4][5] <= ~estado_leds[4][5];
+                estado_leds[4][6] <= ~estado_leds[4][6];
             end
             if (botoes[6]) begin
-                estadoleds[5][5] <= ~estado_leds[5][5];
-                estadoleds[5][6] <= ~estado_leds[5][6];
-                estadoleds[5][7] <= ~estado_leds[5][7];
-                estadoleds[6][5] <= ~estado_leds[6][5];
-                estadoleds[6][6] <= ~estado_leds[6][6];
-                estadoleds[6][7] <= ~estado_leds[6][7];
-                estadoleds[7][5] <= ~estado_leds[7][5];
-                estadoleds[7][6] <= ~estado_leds[7][6];
-                estadoleds[7][7] <= ~estado_leds[7][7];
+                estado_leds[5][5] <= ~estado_leds[5][5];
+                estado_leds[5][6] <= ~estado_leds[5][6];
+                estado_leds[5][7] <= ~estado_leds[5][7];
+                estado_leds[6][5] <= ~estado_leds[6][5];
+                estado_leds[6][6] <= ~estado_leds[6][6];
+                estado_leds[6][7] <= ~estado_leds[6][7];
+                estado_leds[7][5] <= ~estado_leds[7][5];
+                estado_leds[7][6] <= ~estado_leds[7][6];
+                estado_leds[7][7] <= ~estado_leds[7][7];
             end
             if (botoes[7]) begin
-                estadoleds[0][6] <= ~estado_leds[0][6];
-                estadoleds[0][7] <= ~estado_leds[0][7];
-                estadoleds[1][6] <= ~estado_leds[1][6];
-                estadoleds[1][7] <= ~estado_leds[1][7];
-                estadoleds[2][6] <= ~estado_leds[2][6];
-                estadoleds[2][7] <= ~estado_leds[2][7];
-                estadoleds[3][6] <= ~estado_leds[3][6];
-                estadoleds[3][7] <= ~estado_leds[3][7];
-                estadoleds[4][7] <= ~estado_leds[4][7];
+                estado_leds[0][6] <= ~estado_leds[0][6];
+                estado_leds[0][7] <= ~estado_leds[0][7];
+                estado_leds[1][6] <= ~estado_leds[1][6];
+                estado_leds[1][7] <= ~estado_leds[1][7];
+                estado_leds[2][6] <= ~estado_leds[2][6];
+                estado_leds[2][7] <= ~estado_leds[2][7];
+                estado_leds[3][6] <= ~estado_leds[3][6];
+                estado_leds[3][7] <= ~estado_leds[3][7];
+                estado_leds[4][7] <= ~estado_leds[4][7];
             end
         end
     end
